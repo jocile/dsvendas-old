d41f7cb6af525fe53d855a7b0bdd6b9e6a20f492
Jocilé Serra <jocilecam@gmail.com>
2021-11-05 23:17:10 -0300

    Componentes estáticos básicos

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,17 @@
+import DataTable from "components/DataTable";
+import Footer from "components/footer";
+import NavBar from "components/NavBar";
 
 function App() {
   return (
-    <div>
-      <h1 className="text-primary">Olá mundo!</h1>
-    </div>
+    <>
+      <NavBar />
+      <div className="container">
+        <h1 className="text-primary">Olá mundo!</h1>
+        <DataTable />
+      </div>
+      <Footer />
+    </>
   );
 }
 
